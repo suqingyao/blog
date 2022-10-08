2935cea9abdb3bce3029fc15c9da967313d29cf4
cullyfung <fcx.cli@qq.com>
2022-10-08 21:21:41 +0800

    chore: eslint+prettier init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: false,
+  trailingComma: "none",
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "avoid"
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
+const { resolve } = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': resolve(__dirname)
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
