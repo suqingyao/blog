5b5be6944ffc846c5510e6eb7166f5797e741232
cullyfung <fcx.cli@qq.com>
2022-10-10 19:07:46 +0800

    style: refactor style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import 'uno.css'
+import '@unocss/reset/tailwind.css'
+import '@/styles/globals.scss'
+import '@/styles/font.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Layout from '@/components/layout'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Cully Fung Website</title>
+        <meta name="description" content="cully fung personal website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
-export default MyApp
+export default App
