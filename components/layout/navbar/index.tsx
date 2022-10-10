4b5ec65eadfa597659fa56ced82f959e1be603f7
cullyfung <fcx.cli@qq.com>
2022-10-10 19:34:34 +0800

    feat: add navbar

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import Link from 'next/link'
 
-interface NavbarProps {
-  children?: React.ReactNode
+interface LinkProps {
+  url: string
+  title: string
+  icon?: string
 }
 
-const Navbar = ({ children }: NavbarProps) => {
-  return <nav className={'sticky top-0 h-12'}>navbar</nav>
+const Navbar = () => {
+  const linkList: LinkProps[] = [
+    {
+      url: '/projects',
+      title: 'Projects',
+      icon: 'i-ri-lightbulb-line md:hidden'
+    },
+    {
+      url: '/posts',
+      title: 'Blog',
+      icon: 'i-ri-article-line md:hidden'
+    }
+  ]
+
+  return (
+    <nav
+      className={'sticky top-0 h-12 flex justify-end gap-3 align-center px-10'}>
+      {linkList.map(link => (
+        <Link key={link.url} href={link.url}>
+          <div className="flex flex-col justify-center">
+            <span className="lt-md:hidden">{link.title}</span>
+            <div className={link.icon} />
+          </div>
+        </Link>
+      ))}
+    </nav>
+  )
 }
 
 export default Navbar
---
+++ pages/posts/index.tsx
@@ -0,0 +1,5 @@
+const Posts = () => {
+  return <div>POST</div>
+}
+
+export default Posts
