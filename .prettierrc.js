d74a9af18b127d469c05efa4f543fa192a7a4c4e
cullyfung <fcx.cli@qq.com>
2022-10-18 14:53:22 +0800

    chore: update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,11 @@
 module.exports = {
+  printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   singleQuote: true,
   semi: false,
-  trailingComma: "none",
+  trailingComma: 'none',
   bracketSpacing: true,
   bracketSameLine: true,
-  arrowParens: "avoid"
+  arrowParens: 'avoid'
 }
