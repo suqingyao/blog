2b4bc7e127af3eb0fd55a200624cc33771336c21
cullyfung <fcx.cli@qq.com>
2022-10-21 16:37:16 +0800

    feat: add home link

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -26,18 +26,25 @@ const Navbar = () => {
   return (
     <nav
       className={
-        'sticky top-0 h-12 flex justify-end gap-3 align-center px-10 backdrop-blur-sm bg-white/30 dark:bg-[#212121]/30'
+        'sticky top-0 h-12 flex items-center justify-between px-10 backdrop-blur-sm bg-white/30 dark:bg-[#212121]/30'
       }>
-      {linkList.map(link => (
-        <Link key={link.url} href={link.url}>
-          <div className="flex flex-col justify-center cursor-pointer">
-            <span className="lt-md:hidden">{link.title}</span>
-            <div className={link.icon} />
-          </div>
+      <div className="flex items-center">
+        <Link href="/">
+          <i className="i-twemoji-melting-face text-lg"> </i>
         </Link>
-      ))}
-      <GithubLink repo="cullyfung" />
-      <ToggleTheme />
+      </div>
+      <div className="flex justify-end gap-3 align-center">
+        {linkList.map(link => (
+          <Link key={link.url} href={link.url}>
+            <div className="flex flex-col justify-center cursor-pointer">
+              <span className="lt-md:hidden">{link.title}</span>
+              <div className={link.icon} />
+            </div>
+          </Link>
+        ))}
+        <GithubLink repo="cullyfung" />
+        <ToggleTheme />
+      </div>
     </nav>
   )
 }
