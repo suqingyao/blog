da9f1ecd611c03329a835882e487e3d7826f4998
cullyfung <cullyfung@gmail.com>
2022-11-18 11:05:07 +0800

    fix: typo

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <div className={'outfit'}>
+      <h1 className={'text-5xl font-bold flex flex-col gap-1'}>
+        <span>Hi,</span>
+        <span>I&apos;m Cully Fung.</span>
+        <span>Front-end developer.</span>
+      </h1>
+      <div className="mt-6 flex flex-col gap-1 text-base">
+        <span>Currently working at Chongqin. </span>
+        <span>I love coding.</span>
+      </div>
+    </div>
+  )
+}
