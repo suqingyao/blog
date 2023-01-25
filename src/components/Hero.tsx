2e37e16a8797ebbb4bd64726cf48a52edecd2a74
cullyfung <cullyfung@gmail.com>
2023-01-25 12:56:04 +0800

    feat: hero add motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,72 @@
+import { motion } from 'framer-motion'
+
 export default function Hero() {
   return (
-    <div className={'outfit'}>
-      <h1 className={'text-5xl font-bold flex flex-col gap-1'}>
-        <span>Hi,</span>
-        <span>I&apos;m Cully Fung.</span>
-        <span>Front-end developer.</span>
+    <section className={'outfit'}>
+      <h1 className="text-5xl font-bold flex flex-col gap-1">
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 }
+          }}
+        >
+          Hi,
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.1, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 }
+          }}
+        >
+          I&apos;m Cully Fung.
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 }
+          }}
+        >
+          Front-end developer.
+        </motion.p>
       </h1>
       <div className="mt-6 flex flex-col gap-1 text-base">
-        <span>Currently working in Chongqing. </span>
-        <span>I love coding.</span>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.3, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 }
+          }}
+        >
+          Currently working in Chongqing.{' '}
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 }
+          }}
+        >
+          I love coding.
+        </motion.p>
       </div>
-    </div>
+    </section>
   )
 }
