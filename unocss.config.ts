0ca3c8d22970cdbb7165ded51f670c585c6edb5a
cullyfung <cullyfung@gmail.com>
2023-02-18 00:05:18 +0800

    chore: update color config

--- unocss.config.ts
+++ unocss.config.ts
@@ -44,7 +44,18 @@ module.exports = defineConfig({
   shortcuts: {},
   theme: {
     colors: {
-      primary: '#00F183'
+      primary: {
+        50: '#F2FDF9',
+        100: '#E6FCF3',
+        200: '#BFF6E0',
+        300: '#99F1CD',
+        400: '#4DE7A8',
+        DEFAULT: '#00DC82',
+        600: '#00C675',
+        700: '#00844E',
+        800: '#00633B',
+        900: '#004227'
+      }
     },
     animation: {
       keyframes: {
